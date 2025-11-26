13e7b71e6bf0415627d691fa209615bd82c6c6f8
Shreya Jadhav <142742335+Shreyaa-J@users.noreply.github.com>
2025-11-26 16:10:58 +0530

    Enhance SqPatternIncrementing.py with output example
    Added sample output and improved formatting.

---
+++ Number Patterns/SqPatternIncrementing.py
@@ -0,0 +1,17 @@
+n=int(input("Enter num :"))
+count=1
+for i in range(1,(n+1)):
+    for i in range(1,(n+1)):
+        print(count,end=" ")
+        count+=1
+    print()
+
+
+"""
+Sample Output: 
+Enter num :4
+1 2 3 4 
+5 6 7 8 
+9 10 11 12 
+13 14 15 16 
+"""
