7e32e94e3a142cc902433e41728d9c72ff898fe7
Shreya Jadhav <142742335+Shreyaa-J@users.noreply.github.com>
2025-11-26 15:55:43 +0530

    Add inverted triangle number pattern script
    This script prints an inverted triangle pattern of numbers based on user input.

---
+++ Number Patterns/NumPatterninvertedTri.py
@@ -0,0 +1,15 @@
+n=int(input("Enter num : "))
+
+for i in range(n,(1-1),-1):
+    for j in range(1,(i+1)):
+        print(i,end="")
+    print()
+
+"""
+Sample output :
+Enter num : 4
+4444
+333
+22
+1
+"""
